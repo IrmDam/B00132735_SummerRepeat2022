82580d4fc700ff8bed447f58bee3e0e1ba71072e
IrmDam <92793460+IrmDam@users.noreply.github.com>
2022-08-10 21:17:03 +0100

    Changed keys spawning anywhere to random fixed positions

---
+++ SummerRepeat2022/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] keyPrefab;
+    private Vector3 Min;
+    private Vector3 Max;
+    private float _xAxis;
+    private float _zAxis;
+    private Vector3 _randomPosition;
+    private float _xAxis1;
+    private float _zAxis1;
+    private Vector3 _randomPosition1;
+    public bool _canInstantiate;
+
+    public Vector3[] spawnPoints;
+
+public static void spawnPosition()
+{
+    Vector3[] spawnPoints = new Vector3[4];
+    
+    Vector3 spawnPos = new Vector3(12, -3f, 21.3f);
+    Vector3 spawnPos1 = new Vector3(-10.6f, -3f, 19.16f);
+    Vector3 spawnPos2 = new Vector3(13.9f, -3f, .34f);
+    spawnPoints[0] = spawnPos;
+    spawnPoints[1] = spawnPos1;
+    spawnPoints[2] = spawnPos2;
+}
+
+
+    //public Vector3 spawnPoints[] = (new Vector3(12, -3f, 21.3f), new Vector3(-10.6f, -3f, 19.16f));
+   //public Vector3 spawnPos2 = new Vector3(-10.6f, -3f, 19.16f);
+
+    
+
+   /* Transform GetSpawnPoint()
+    {
+        int locationIndex = Random.Range(0, spawnPoints.length);
+        return spawnPoints[locationIndex];
+    }
+*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPosition();
+        /*SetRanges();
+         _xAxis = UnityEngine.Random.Range(Min.x, Max.x);
+         _zAxis = UnityEngine.Random.Range(Min.z, Max.z);
+         _xAxis1 = UnityEngine.Random.Range(Min.x, Max.x);
+         _zAxis1 = UnityEngine.Random.Range(Min.z, Max.z);
+        _randomPosition = new Vector3(_xAxis, -3f, _zAxis );
+        _randomPosition1 = new Vector3(_xAxis1, -3f, _zAxis1);
+        */
+        Instantiate(keyPrefab[0], spawnPoints[2], Quaternion.identity);
+        //Instantiate(keyPrefab[1], _randomPosition1, Quaternion.identity);
+    }
+
+    private void SetRanges()
+     {
+         Min = new Vector3(14, -2, 0); 
+         Max = new Vector3(-15.5f, -2, 30);
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
