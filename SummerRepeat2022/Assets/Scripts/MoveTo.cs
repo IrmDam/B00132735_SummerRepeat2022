252048dc61e975f853f1f032f6431517cc39e481
IrmDam <92793460+IrmDam@users.noreply.github.com>
2022-08-10 02:52:25 +0100

    Started working on enemy

---
+++ SummerRepeat2022/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,19 @@
+// MoveTo.cs
+    using UnityEngine;
+    using UnityEngine.AI;
+    
+    public class MoveTo : MonoBehaviour {
+       
+      private var targets : GameObject[];
+      private var navigation : NavMeshAgent;
+      private var i : int = 0;
+
+       public Transform goal;
+       public Transform goal2;
+       
+       void Start () {
+          NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+
+       }
+    }
